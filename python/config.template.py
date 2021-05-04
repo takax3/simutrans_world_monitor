543687c45c491c9861f2a9dd5e2b10bfedc546b3
ahakuoku <ahakuoku@gmail.com>
2021-05-04 17:03:38 +0900

    このコミットはミス　後で消す

--- python/config.template.py
+++ python/config.template.py
@@ -5,9 +5,6 @@ TOKEN = r"fugafuga"
 CHANNEL_ID = 1145141919810
 DIRECTORY = r"/Users/XXX/simutrans"
 
-TEXT_HELLO_TITLE = 'botを起動しました。'
-TEXT_HELLO_DESC  = '使用方法は`?help`と入力してください。'
-TEXT_BUSY_TITLE  = '現在別の処理を実行中です。'
-TEXT_BUSY_DESC   = 'しばらくしてからもう一度やりなおしてください。'
-TEXT_WAIT_TITLE  = '処理中です。しばらくお待ちください。'
-TEXT_WAIT_DESC   = '処理が終わらない場合はサーバーダウンもしくはロード中の可能性があります。\nこの処理が終了するまで一部を除くコマンドは実行できません。'
+TEXT_HELLO = 'botを起動しました。使用方法は`?help`と入力してください。'
+TEXT_BUSY  = '処理に失敗しました。'
+TEXT_WAIT  = '処理中です。しばらくお待ちください。'
