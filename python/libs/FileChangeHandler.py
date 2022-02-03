c0a59d1a4db4c3ade103f1d1c197ce27598f2cf9
ahakuoku <ahakuoku@gmail.com>
2022-02-03 22:01:46 +0900

    文字数制限を超過した際の分割送信に対応

--- python/libs/FileChangeHandler.py
+++ python/libs/FileChangeHandler.py
@@ -1,3 +1,5 @@
+import enum
+from typing import Dict
 import config
 import os
 import hashlib
@@ -10,13 +12,18 @@ from libs.vars import *
 FILE_OUT = config.DIRECTORY + '/file_io/out.txt'
 FILE_EMBED = config.DIRECTORY + '/file_io/out_embed.json'
 
+DISCORD_MAX_TEXT_LENGTH = 1950
+
 class FileChangeHandler(FileSystemEventHandler):
+    def discord_text_escape(self, str: str) -> str:
+        return str.translate(str.maketrans({'*': '\\*', '_': '\\_', '~': '\\~', '|': '\\|', '`': '\\`', ':': '\\:'}))
+    
     def remove_waiting_message(self, ch):
         if get_waiting_message()!=None:
             coro = ch.delete_messages([get_waiting_message()])
             asyncio.run_coroutine_threadsafe(coro, client.loop)
             set_waiting_message(None)
-            
+    
     def is_same_message(self, str):
         hash = hashlib.md5(str.encode()).hexdigest()
         if hash==get_prev_out_hash():
@@ -27,14 +34,29 @@ class FileChangeHandler(FileSystemEventHandler):
     
     def plain_text_out(self):
         with open(FILE_OUT, encoding='utf-8') as f:
-            s = f.read()[:1990]
+            s = self.discord_text_escape(f.read())
             if s=='empty' or self.is_same_message(s):
                 return
             ch = client.get_channel(config.CHANNEL_ID)
             self.remove_waiting_message(ch)
-            coro = ch.send(s)
-            asyncio.run_coroutine_threadsafe(coro, client.loop)
-                            
+            if s <= DISCORD_MAX_TEXT_LENGTH :
+                coro = ch.send(s)
+                asyncio.run_coroutine_threadsafe(coro, client.loop)
+            else:
+                s_sprited: list = s.split('\n')
+                message_list: list = []
+                message: str = ""
+                for line in s_sprited:
+                    if len(message) + len(line) >= DISCORD_MAX_TEXT_LENGTH :
+                        message_list += [message]
+                        message = ""
+                    message += line + '\n'
+                else:
+                    message_list += [message]
+                for i, message in enumerate(message_list):
+                    coro = ch.send(f'(Part {i + 1}/{len(message_list)})\n``{message}``')
+                    asyncio.run_coroutine_threadsafe(coro, client.loop)
+    
     def embed_out(self):
         with open(FILE_EMBED, encoding='utf-8') as f:
             s = f.read()
@@ -43,15 +65,36 @@ class FileChangeHandler(FileSystemEventHandler):
             ch = client.get_channel(config.CHANNEL_ID)
             self.remove_waiting_message(ch)
             jo = json.JSONDecoder().raw_decode(s)[0]
-            embed = discord.Embed(title=jo["title"], description=jo["description"], color=jo["color"])
-            if jo["fields"]:
-                for f in jo["fields"]:
-                    embed.add_field(name=f["name"], value=f["value"], inline=True)
-            if jo["footer"]:
-                embed.set_footer(text=jo["footer"])
-            coro = ch.send(embed=embed)
-            asyncio.run_coroutine_threadsafe(coro, client.loop)
-        
+            if len(jo["description"]) <= DISCORD_MAX_TEXT_LENGTH:
+                embed = discord.Embed(title=jo["title"], description=self.discord_text_escape(jo["description"]), color=jo["color"])
+                if jo["fields"]:
+                    for f in jo["fields"]:
+                        embed.add_field(name=f["name"], value=f["value"], inline=True)
+                if jo["footer"]:
+                    embed.set_footer(text=jo["footer"])
+                coro = ch.send(embed=embed)
+                asyncio.run_coroutine_threadsafe(coro, client.loop)
+            else:
+                desc_sprited: list = self.discord_text_escape(jo["description"]).split('\n')
+                message_list: list = []
+                message: str = ""
+                for line in desc_sprited:
+                    if len(message) + len(line) >= DISCORD_MAX_TEXT_LENGTH :
+                        message_list += [message]
+                        message = ""
+                    message += line + '\n'
+                else:
+                    message_list += [message]
+                for i, message in enumerate(message_list):
+                    embed = discord.Embed(title=jo["title"] + f" (Part {i + 1}/{len(message_list)})", description=message, color=jo["color"])
+                    if jo["fields"]:
+                        for f in jo["fields"]:
+                            embed.add_field(name=f["name"], value=f["value"], inline=True)
+                    if jo["footer"]:
+                        embed.set_footer(text=jo["footer"])
+                    coro = ch.send(embed=embed)
+                    asyncio.run_coroutine_threadsafe(coro, client.loop)
+    
     # ファイル変更時のイベント
     def on_modified(self, event):
         filepath = event.src_path
