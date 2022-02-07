24f85e38d95dc90d9e352ccb0daab03b5c3c51d0
taka_taka_taka <takax3taka@gmail.com>
2022-02-07 23:52:09 +0900

    add 'reset' command

--- python/config.template.py
+++ python/config.template.py
@@ -4,6 +4,7 @@
 TOKEN = r"fugafuga"
 CHANNEL_ID = 1145141919810
 DIRECTORY = r"/Users/XXX/simutrans"
+ADMIN = 1145141919810
 
 TEXT_HELLO_TITLE = 'botを起動しました。'
 TEXT_HELLO_DESC  = '使用方法は`?help`と入力してください。'
@@ -11,3 +12,5 @@ TEXT_BUSY_TITLE  = '現在別の処理を実行中です。'
 TEXT_BUSY_DESC   = 'しばらくしてからもう一度やりなおしてください。'
 TEXT_WAIT_TITLE  = '処理中です。しばらくお待ちください。'
 TEXT_WAIT_DESC   = '処理が終わらない場合はサーバーダウンもしくはロード中の可能性があります。\nこの処理が終了するまで一部を除くコマンドは実行できません。'
+TEXT_RESET_TITLE_NOT_ADMIN = '処理のリセットが行えるのはAdminのみです。'
+TEXT_RESET_DESC_NOT_ADMIN  = '適当に扱われるとどうなるかわからないので制限してるんや　すまんやで'
--- python/monitor.py
+++ python/monitor.py
@@ -24,12 +24,20 @@ async def on_ready():
 async def on_message(message):
     channel = client.get_channel(config.CHANNEL_ID)
     # 指定チャンネルでの指定フォーマットの人間のメッセージのみ反応
-    content = message.content.replace('？','?').replace('，',',').replace('、',',').replace('knAHNS','これなにAHNS')
+    content: str = message.content.replace('？', '?').replace('，', ',').replace('、', ',').replace('ヘルプ', 'help').replace('knAHNS', 'これなにAHNS')
     AHNS_dictionary = content.find('?これなにAHNS')
     if message.author.bot or message.channel != channel or content[0]!='?' or len(content)<2:
         return
     elif content in help.helps :
         await channel.send(embed=help.helps[content])
+        return
+    elif content.lower().startswith('?reset') :
+        if message.author.id != config.ADMIN:
+            await channel.send(embed=discord.Embed(title=config.TEXT_RESET_TITLE_NOT_ADMIN, description=config.TEXT_RESET_DESC_NOT_ADMIN, color=0xff0000))
+        else:
+            with open(FILE_CMD, mode='w', encoding='utf-8') as f:
+                f.write('empty')
+            await channel.send(embed=discord.Embed(title=config.TEXT_RESET_TITLE, description=config.TEXT_RESET_DESC, color=0xff0000))
         return
     elif AHNS_dictionary == 0 :
         knAHNS_1, knAHNS_2 = content.split(',', 1)
