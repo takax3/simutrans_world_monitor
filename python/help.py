36539bc3440798f2103f7d5c8bc2951d959da01f
ahakuoku <ahakuoku@gmail.com>
2022-07-20 15:42:23 +0900

    削除：knAHNSと隠しコマンド

--- python/help.py
+++ python/help.py
@@ -5,7 +5,7 @@ import discord
 
 helps = {}
 
-helps['?help'] = discord.Embed(title="コマンド一覧", description="本botで使用できるコマンドは次の通りです。\n※helpコマンド・knAHNSコマンド・addressコマンド以外はSimutrans起動時以外使用できません。\n引数の区切りはコンマ(`,`)で行います。\n`<>`のカッコ内は、その引数が必須であることを示します。（カッコは外してください）\n`[]`のカッコ内は、その引数がオプションであることを示します。（カッコは外してください）", color=0x00ff00)
+helps['?help'] = discord.Embed(title="コマンド一覧", description="本botで使用できるコマンドは次の通りです。\n※helpコマンド・addressコマンド以外はSimutrans起動時以外使用できません。\n引数の区切りはコンマ(`,`)で行います。\n`<>`のカッコ内は、その引数が必須であることを示します。（カッコは外してください）\n`[]`のカッコ内は、その引数がオプションであることを示します。（カッコは外してください）", color=0x00ff00)
 helps['?help'].add_field(name="`?help`", value="本botのヘルプを返します。", inline=False)
 helps['?help'].add_field(name="`?wait,<駅名>,[エントリ数]`", value="指定された停車場の待機客およびその行き先を返します。エントリ数のデフォルト値は5です。", inline=False)
 helps['?help'].add_field(name="`?player`", value="ゲームに参加している会社一覧と、その番号を返します。ここで返される番号は、プレイヤー番号を指定するコマンドで使用します。", inline=False)
@@ -14,8 +14,7 @@ helps['?help'].add_field(name="`?time`", value="現在の年と月を返しま�
 helps['?help'].add_field(name="`?finance`", value="各会社の資金を返します。", inline=False)
 helps['?help'].add_field(name="`?route,<プレイヤー番号>,[路線属性]`", value="指定したプレイヤーの路線番号と路線名を一覧で返します。", inline=False)
 helps['?help'].add_field(name="`?halt,<路線番号>,[停車場数]`", value="指定した路線のスケジュール上にある停車駅を順番に返します。3つ目のパラメーターを指定すると、路線上にある駅を利用者順でソートして返します。", inline=False)
-helps['?help'].add_field(name="`?knAHNS,<記事名/list>,[カテゴリ名]`", value="指定した名称の記事を返します。listオプションは、指定したカテゴリ内の記事一覧を返します。", inline=False)
-helps['?help'].add_field(name="`?address`", value="接続先を返します。", inline=False)
+helps['?help'].add_field(name="`?ip`", value="接続先を返します。", inline=False)
 
 helps['?接続先'] = discord.Embed(title="接続先情報", value="​", description="ahns.simutrans.info:65535", color=0x00ff00)
 helps['?接続先'].set_footer(text="2021年12月現在")
@@ -23,95 +22,3 @@ helps['?address'] = discord.Embed(title="接続先情報", value="​", descript
 helps['?address'].set_footer(text="2021年12月現在")
 helps['?ip'] = discord.Embed(title="接続先情報", value="​", description="ahns.simutrans.info:65535", color=0x00ff00)
 helps['?ip'].set_footer(text="2021年12月現在")
-helps['?これなにAHNS'] = discord.Embed(title="記事名を指定してください。", value="​", description="このコマンドの正しい使用方法は、`?これなにAHNS,<記事名/list>,[カテゴリー]`です。\nコマンド文にスペースが入っていたりすると正しく動作しません。", color=0xff0000)
-# この先隠しコマンドあり！閲覧注意！
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-helps['?ahakuoku'] = discord.Embed(title="ahakuoku", description="ahakuokuさんは、128jpの公開鯖の鯖主としてAHNSの創設に尽力し、AHNSに自由の雰囲気を築くために多大な功績を残した人です。どうかこのメッセージを消さないでください。", color=0x00ff00)
-
-helps['?かわぎえ'] = discord.Embed(title="かわぎえ", description="ぎえーｗｗｗｗｗ", color=0x00ff00)
-
-helps['?お前を消す方法'] = discord.Embed(title="お前を消す方法", description="邪魔だったよね、ごめんね。もう少し役に立てるように頑張るよ。", color=0x00ff00)
-
-# helps['?鯖主の最寄り駅'] = discord.Embed(title="お前を消す方法", description="<:kamioooka_A:800358960325853254><:stop:800361810506022972>\n<:kamioooka_B:800359063513858058><:stop:800361810506022972>", color=0x00ff00)
-
-# helps['?OTNP'] = discord.Embed(title="OTNP", description="でっけえ男性器 飛んでるよ 飛んでるよ", color=0x00ff00)
-
-helps['?デドロイト市警だ！'] = discord.Embed(title="デドロイト市警だ！", description="君？嘘をつくのは、一体良くない。", color=0x00ff00)
-
-# helps['?ヘイト行為'] = discord.Embed(title="ヘイト行為", description="仏塔鉄道は1等旅客専業となりました。全謬爆劣2等旅客はバスでもﾂｶｯﾃﾛ", color=0x00ff00)
-
-# helps['?イオン'] = discord.Embed(title="イオン", description="ヘイト企業イオンは謝罪しろ！", color=0x00ff00)
-
-# helps['?高鷲'] = discord.Embed(title="高鷲", description="ここで一句\n\nちんちんは\n​　いくら揉んでも\n​　　かまいません", color=0x00ff00)
-
-helps['?かわごえ'] = discord.Embed(title="かわごえ", description="かわごえさんは、AHNSの事実上の副鯖主として、AHNSの発展に尽力し、AHNSを大手NS鯖とするために多大な功績を残した人です。", color=0x00ff00)
--- python/monitor.py
+++ python/monitor.py
@@ -5,7 +5,6 @@ import discord
 import os
 import config
 import help
-import knAHNS
 from watchdog.observers import Observer
 from libs.FileChangeHandler import FileChangeHandler
 from libs.vars import *
@@ -24,7 +23,7 @@ async def on_ready():
 async def on_message(message):
     channel = client.get_channel(config.CHANNEL_ID)
     # 指定チャンネルでの指定フォーマットの人間のメッセージのみ反応
-    content: str = message.content.replace('？', '?').replace('，', ',').replace('、', ',').replace('ヘルプ', 'help').replace('knAHNS', 'これなにAHNS')
+    content: str = message.content.replace('？', '?').replace('，', ',').replace('、', ',').replace('ヘルプ', 'help')
     AHNS_dictionary = content.find('?これなにAHNS')
     if message.author.bot or message.channel != channel or content[0]!='?' or len(content)<2:
         return
@@ -39,15 +38,6 @@ async def on_message(message):
                 f.write('empty')
             await channel.send(embed=discord.Embed(title=config.TEXT_RESET_TITLE, description=config.TEXT_RESET_DESC, color=0x00ff00))
         return
-    elif AHNS_dictionary == 0 :
-        knAHNS_1, knAHNS_2 = content.split(',', 1)
-        if knAHNS_2 in knAHNS.knAHNSs:
-            await channel.send(embed=knAHNS.knAHNSs[knAHNS_2])
-            return
-        else:
-            embed=discord.Embed(title="指定された名前の記事はありません。", description="このコマンドの正しい使用方法は、`?これなにAHNS,<記事名/list>,[カテゴリー]`です。\nコマンド文にスペースが入っていたりすると正しく動作しません。", color=0xff0000)
-            await channel.send(embed=embed)
-            return
     with open(FILE_CMD, encoding='utf-8') as f:
         s = f.read()
         if s and not s.startswith('empty'):
